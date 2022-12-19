71d1beb7727cef5a99805720ddb2f66ef9de26b1
aisowinka <100514174+aisowinka@users.noreply.github.com>
2022-12-19 20:03:15 +0300

    Add files via upload

---
+++ Лаб1/Лаб1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Лаб1
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.checkBtn = new System.Windows.Forms.Button();
+            this.richTB = new System.Windows.Forms.RichTextBox();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.tbTime = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.labelCheck = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(67, 488);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(195, 46);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Начать";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // checkBtn
+            // 
+            this.checkBtn.Location = new System.Drawing.Point(498, 488);
+            this.checkBtn.Name = "checkBtn";
+            this.checkBtn.Size = new System.Drawing.Size(195, 46);
+            this.checkBtn.TabIndex = 0;
+            this.checkBtn.Text = "Проверить";
+            this.checkBtn.UseVisualStyleBackColor = true;
+            this.checkBtn.Click += new System.EventHandler(this.checkBtn_Click);
+            // 
+            // richTB
+            // 
+            this.richTB.BackColor = System.Drawing.Color.White;
+            this.richTB.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTB.Location = new System.Drawing.Point(12, 12);
+            this.richTB.Name = "richTB";
+            this.richTB.ReadOnly = true;
+            this.richTB.Size = new System.Drawing.Size(250, 276);
+            this.richTB.TabIndex = 1;
+            this.richTB.Text = "";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(285, 501);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(65, 21);
+            this.labelTime.TabIndex = 2;
+            this.labelTime.Text = "Время:";
+            // 
+            // tbTime
+            // 
+            this.tbTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbTime.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbTime.Location = new System.Drawing.Point(356, 501);
+            this.tbTime.Name = "tbTime";
+            this.tbTime.ReadOnly = true;
+            this.tbTime.Size = new System.Drawing.Size(28, 24);
+            this.tbTime.TabIndex = 3;
+            this.tbTime.Text = "15";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 21;
+            this.listBox.Location = new System.Drawing.Point(467, 9);
+            this.listBox.Name = "listBox";
+            this.listBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox.Size = new System.Drawing.Size(233, 277);
+            this.listBox.TabIndex = 6;
+            // 
+            // labelCheck
+            // 
+            this.labelCheck.AutoSize = true;
+            this.labelCheck.ForeColor = System.Drawing.Color.DarkRed;
+            this.labelCheck.Location = new System.Drawing.Point(426, 303);
+            this.labelCheck.Name = "labelCheck";
+            this.labelCheck.Size = new System.Drawing.Size(0, 21);
+            this.labelCheck.TabIndex = 5;
+            this.labelCheck.Visible = false;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.labelCheck);
+            this.Controls.Add(this.tbTime);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.richTB);
+            this.Controls.Add(this.checkBtn);
+            this.Controls.Add(this.startBtn);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.Name = "mainForm";
+            this.Text = "Лабораторная работа № 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button checkBtn;
+        private System.Windows.Forms.RichTextBox richTB;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.TextBox tbTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label labelCheck;
+    }
+}
+
---
+++ Лаб1/Лаб1/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаб1
+{
+    public partial class mainForm : Form
+    {
+        int step = 0;
+        List<int> intRomain = new List<int>();
+        string[] romain = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+        bool colorIsActive = false;
+        bool again = false;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            checkBtn.Enabled = false;
+            labelTime.Enabled = false;
+            tbTime.Enabled = false;
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            int value;
+            MessageBox.Show("Дорогой друг. Тебе тебе будет дано 15 секунд, чтобы запомнить цифры.");
+            labelCheck.Text = "";
+
+            Random rnd = new Random();
+            intRomain.Clear();
+            tbTime.Text = "15";
+
+            while(count < 7)
+            {
+                value = rnd.Next(1, 9);
+                if (!intRomain.Contains(value))
+                {
+                    intRomain.Add(value);
+                    count++;
+                }
+            }
+
+            if (step == 0)
+            {
+                foreach (int i in intRomain)
+                {
+                    if (colorIsActive && i % 2 == 0)
+                        richTB.SelectionColor = Color.Red;
+                    richTB.AppendText(romain[i] + "\n");
+                    richTB.SelectionColor = richTB.SelectionColor = richTB.ForeColor;
+                }
+            }
+            else if(step == 1)
+            {
+                string s;
+                foreach (int i in intRomain)
+                {
+                    s = "";
+                    for(int j = i; 0 < j; j--)
+                    {
+                        s += "*";
+                    }
+                    richTB.AppendText(s + "\n");
+                }
+            }
+            startBtn.Enabled = false;
+            labelTime.Enabled = true;
+            tbTime.Enabled = true;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int time = Convert.ToInt32(tbTime.Text);
+            if (time == 0)
+            {
+                timer1.Stop();
+                labelTime.Enabled = false;
+                tbTime.Enabled = false;
+                richTB.Text = "";
+                labelCheck.Visible = true;
+                checkBtn.Enabled = true;
+                labelCheck.Text = "Выберите числа, которые вы запомнили";
+                if (step == 0)
+                {
+                    for (int i = 1; i <= 9; i++)
+                    {
+                        listBox.Items.Add(romain[i]);
+                    }
+                }
+                else if( step == 1)
+                {
+                    string s;
+                    for (int i = 1; i <= 9; i++)
+                    {
+                        s = "";
+                        for(int j = i; 0 < j; j--)
+                        {
+                            s += "*";
+                        }
+                        listBox.Items.Add(s);
+                    }
+                }
+            }
+            else
+            {
+                tbTime.Text = (time - 1).ToString();
+            }
+        }
+
+        private void checkBtn_Click(object sender, EventArgs e)
+        {
+            int countOfRight = 0;
+            if(listBox.SelectedItems.Count == 7)
+            {
+                foreach(string s in listBox.SelectedItems)
+                {
+                    if (step == 0)
+                    {
+                        if (intRomain.Contains(Array.IndexOf(romain, s)))
+                        {
+                            countOfRight++;
+                        }
+                    }
+                    else if(step == 1)
+                    {
+                        if (intRomain.Contains(s.Length))
+                        {
+                            countOfRight++;
+                        }
+                    }
+
+                }
+                MessageBox.Show("Правильных ответов: " + countOfRight.ToString() + "\n" + "Неправильных ответов: " + (7 - countOfRight).ToString());
+                listBox.Items.Clear();
+                step++;
+                startBtn.Text = "Шаг " + (step + 1);
+                startBtn.Enabled = true;
+                checkBtn.Enabled = false;
+                if(colorIsActive)
+                {
+                    startBtn.Text = "Начать";
+                    colorIsActive = false;
+                    step = 0;
+                    again = false;
+                }
+                if (again)
+                {
+                    startBtn.Text = "Шаг " + 4;
+                    colorIsActive = true;
+                    step = 0;
+                }
+
+                if(step == 2)
+                {
+                    step = 0;
+                    again = true;
+                }
+                    
+
+            }
+            else
+            {
+                labelCheck.Text = "Выберите ровно 7 элементов!";
+            }
+        }
+    }
+}
